6553d31a2f5b46a4e321cfe9ebbda63eadae99ad
Aryan pal <aryan.pal01@gmail.com>
2023-12-12 21:52:18 +0530

    Done with the Post and comment view

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,15 @@
+class CommentsController < ApplicationController
+  def create
+    begin
+      @post = Post.find(params[:post_id])
+      @comment =  @post.comments.create(comment_params);
+      redirect_to post_path(@post)
+    rescue StandardError => exception
+      puts "ERROR #{exception}"
+    end
+   end
+    private
+    def comment_params
+      params.require(:comment).permit(:commenter, :body)
+    end
+end
